dc07eb9eecb7ff71cf51e387214f44d7f6a8d79c
Nate <jeditendencies35@gmail.com>
2023-08-23 18:45:46 -0400

    started building this nhl app thing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,5 @@
 function loadScript() {
 
-  const singleTeam = document.querySelector('.single-team');
-  const singleTeamList = document.querySelector('.single-team-list');
-  const rosterSelectBox = document.getElementById('roster-select-box');
-  const teamRosterContainer = document.querySelector('.team-roster-container');
-  const playerStats = document.querySelector('.player-stats');
-
   const allTeamsDropdownButton = document.querySelector('.all-teams-dropdown-button');
   const allTeamsDropdownList = document.querySelector('.all-teams-dropdown-list');
   const teamRosterDropdownButton = document.querySelector('.team-roster-dropdown-button');
@@ -15,20 +9,30 @@ function loadScript() {
   const team = document.querySelector('.team');
   const teamConference = document.querySelector('.team-conference');
   const teamDivision = document.querySelector('.team-division');
-  const teamArena = document.querySelector('.team-arena');
+  const teamVenue = document.querySelector('.team-venue');
   const teamSite = document.querySelector('.team-site');
   const teamLogo = document.querySelector('.team-logo');
+
+  const playerStatsContainer = document.querySelector('.player-stats-container');
+  const playerName = document.querySelector('.player-name');
+  const playerHeight = document.querySelector('.player-height');
+  const playerWeight = document.querySelector('.player-weight');
+  const playerAge = document.querySelector('.player-age');
+  const playerDOB = document.querySelector('.player-dob');
+  const playerBirthplace = document.querySelector('.player-birthplace');
   // default api
   const api = {
     baseUrl: 'https://statsapi.web.nhl.com/api/v1'
   };
-  // gets all data
+
+  // gets all teams
   async function getAllTeams() {
     const res = await fetch(`${api.baseUrl}/teams`);
     const data = await res.json();
     return data;
   }
-  // gets all teams for dropdown
+
+  // populates all teams dropdown
   async function populateTeamSelection() {
     const data = await getAllTeams();
     allTeamsDropdownList.innerHTML = data.teams.map(team => `
@@ -38,24 +42,25 @@ function loadScript() {
     teamDropdownName.forEach((teamName) => {
       teamName.addEventListener('click', (e) => {
         showSingleTeam(e.target.innerText);
-        // console.log(e.target.innerText);
+        setTimeout(() => {
+          playerStatsContainer.classList.remove('single-team-container-toggle');
+        }, 250);
       });
     });
   }
+
   // gets single team data
   async function showSingleTeam(teamDropdownSelection) {
     const data = await getAllTeams();
     const selectedTeam = teamDropdownSelection;
-
     for (let i = 0; i < data.teams.length; i++) {
       if (selectedTeam === data.teams[i].name) {
         team.innerText = data.teams[i].name;
         teamConference.innerText = data.teams[i].conference.name;
         teamDivision.innerText = data.teams[i].division.name;
-        teamArena.innerText = data.teams[i].venue.name;
+        teamVenue.innerText = data.teams[i].venue.name;
         teamSite.innerHTML = `<a href='${data.teams[i].officialSiteUrl}' target='_blank'><span>${data.teams[i].officialSiteUrl}</span></a>`;
         teamLogo.innerHTML = `<img src='img/${data.teams[i].name.normalize('NFD').replace(/[\u0300-\u036f]/g, "")}.png'>`;
-
         populateTeamRoster(data.teams[i].id);
         setTimeout(() => {
           teamRosterDropdownList.classList.add('dropdown-list-toggle');
@@ -64,24 +69,37 @@ function loadScript() {
       }
     }
   }
-  // gets team roster data
+
+  // populates team roster dropdown
   async function populateTeamRoster(teamID) {
     const res = await fetch(`${api.baseUrl}/teams/${teamID}/roster`);
     const data = await res.json();
     teamRosterDropdownList.innerHTML = data.roster.map(players => `
-    <li class='roster-dropdown-name'>${players.person.fullName}</li>
+    <li id='${players.person.id}' class='roster-dropdown-name'>${players.person.fullName}</li>
     `).sort().join('');
     let rosterDropdownName = document.querySelectorAll('.roster-dropdown-name');
     rosterDropdownName.forEach((rosterName) => {
       rosterName.addEventListener('click', (e) => {
-        getPlayer(e.target.innerText);
+        getPlayer(e.target.getAttribute('id'));
+        // teamRosterDropdownList.classList.remove('dropdown-list-toggle');
       });
     });
   }
+
   // gets single player data
-  async function getPlayer(rosterDropdownSelection) {
-    const selectedPlayer = rosterDropdownSelection;
-    console.log(selectedPlayer);
+  async function getPlayer(id) {
+    const response = await fetch(`${api.baseUrl}/people/${id}`);
+    const data = await response.json();
+    playerName.innerText = data.people[0].fullName;
+    playerHeight.innerText = data.people[0].height;
+    playerWeight.innerText = `${data.people[0].weight} lbs`;
+    playerAge.innerText = data.people[0].currentAge;
+    playerDOB.innerText = data.people[0].birthDate;
+    playerBirthplace.innerText = `${data.people[0].birthCity}, ${data.people[0].birthCountry}`;
+    setTimeout(() => {
+      playerStatsContainer.classList.add('single-team-container-toggle');
+    }, 250);
+    // console.log(data);
   }
 
   allTeamsDropdownButton.addEventListener('click', () => {
@@ -90,9 +108,9 @@ function loadScript() {
   teamRosterDropdownButton.addEventListener('click', () => {
     teamRosterDropdownList.classList.toggle('dropdown-list-toggle');
   });
-
   populateTeamSelection();
 }
+
 window.addEventListener('load', () => {
   loadScript();
 });
